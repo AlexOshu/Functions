69e31bd7bd9d6503dc7f7d2d7c2b3f5642fa82b8
Александр Ошурков <aleksanro@yandex.ru>
2022-07-08 22:00:41 +0300

    Functions basics

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -2,8 +2,39 @@
 #include<iostream>
 using namespace std;
 
+int Add(int a, int b);
+int Sub(int a, int b);
+int Mul(int a, int b);
+double Div(int a, int b);
+
+
 void main()
 {
 	setlocale(LC_ALL, "");
-
+	int a, b;
+	cout << "¬ведите два числа: "; cin >> a >> b;
+	int c = Add(a, b);// »спользование фунции или вызов (Function call)
+	cout << a << "+" << b << "=" << c << endl;
+	cout << a << "-" << b << "=" << Sub(a, b) << endl;
+	cout << a << "*" << b << "=" << Mul(a, b) << endl;
+	cout << a << "/" << b << "=" << Div(a, b) << endl;
 }
+
+int Add(int a, int b)
+{
+	int c = a + b;
+	return c;
+}
+int Sub(int a, int b)
+{
+	return a - b;
+}
+int Mul(int a, int b)
+{
+	return a * b;
+}
+double Div(int a, int b)
+{
+	return (double)a / b;
+}
+
